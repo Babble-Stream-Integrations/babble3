8c5889ca5f98b63af18af5c1ce31f7e0a94bbd26
Lvdwardt <87395969+Lvdwardt@users.noreply.github.com>
2022-09-19 21:06:43 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App() {
         <h1 className="font-bold text-transparent text-4xl bg-clip-text bg-gradient-to-r from-babbleRed to-babbleYellow">
           Babble
         </h1>
+        <h2 className="text-white">test</h2>
       </div>
       <div className="absolute left-8 bottom-8">
         <img src={logo} className="w-10 h-10" alt="logo" />
