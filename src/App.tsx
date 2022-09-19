2a05ae4c6af21c626fc555db45f62d9db60843b3
Lvdwardt <87395969+Lvdwardt@users.noreply.github.com>
2022-09-19 19:40:43 +0200

    tailwind babble colors and default font

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import React from "react";
+import logo from "./assets/logo-small.png";
 
 function App() {
   return (
-    <div className=" bg-[#0E0E10]">
-      <header className="App-header">
-        <div className="flex flex-1 justify-center h-screen items-center">
-          <h1 className="font-bold text-transparent text-4xl bg-clip-text bg-gradient-to-r from-[#FF0000] to-[#FFC600]">
-            Babble
-          </h1>
-        </div>
-      </header>
-    </div>
+    <header className="App-header bg-babbleBlack">
+      <div className="flex flex-1 justify-center h-screen items-center flex-col">
+        <h1 className="font-bold text-transparent text-4xl bg-clip-text bg-gradient-to-r from-babbleRed to-babbleYellow">
+          Babble
+        </h1>
+      </div>
+      <div className="absolute left-8 bottom-8">
+        <img src={logo} className="w-10 h-10" alt="logo" />
+      </div>
+    </header>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,28 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    //change font family
+    fontFamily: {
+      sans: ["Montserrat", "sans-serif"],
+    },
+    extend: {
+      //add default babble colors
+      colors: {
+        //greys
+        babbleBlack: "#0E0E10",
+        babbleDarkgray: "#1A1A1D",
+        babbleGray: "#212125",
+        babbleLightgray: "#D0D0D2",
+        babbleWhite: "#F2F2F4",
+        //colors
+        babbleRed: "#FF0000",
+        babbleOrange: "#FF8400",
+        babbleYellow: "#FFC600",
+        babbleDarkblue: "#0062FF",
+        babbleBlue: "#007BFF",
+        babbleLightblue: "#00F1FF",
+      },
+    },
   },
   plugins: [],
 };
