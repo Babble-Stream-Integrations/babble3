2106d39b12659240afd47de8b4b1a812db338c73
Lvdwardt <87395969+Lvdwardt@users.noreply.github.com>
2022-09-19 10:26:19 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className=" bg-[#0E0E10]">
+      <header className="App-header">
+        <div className="flex flex-1 justify-center h-screen items-center">
+          <h1 className="font-bold text-transparent text-4xl bg-clip-text bg-gradient-to-r from-[#FF0000] to-[#FFC600]">
+            Babble
+          </h1>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
