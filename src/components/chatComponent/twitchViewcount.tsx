f40b8ff40fd2308735d7216025ae724b26edfbf4
Lvdwardt <87395969+Lvdwardt@users.noreply.github.com>
2022-09-23 13:30:59 +0200

    make first viewcount fetch immediately

--- src/components/chatComponent/twitchViewcount.tsx
+++ src/components/chatComponent/twitchViewcount.tsx
@@ -7,10 +7,25 @@ interface Props {
   };
   setViewCount: React.Dispatch<React.SetStateAction<string>>;
 }
-export default function TwitchViewcount({ streamer, setViewCount }: Props) {
+
+export default function TwitchViewCount({ streamer, setViewCount }: Props) {
   const myHeaders = new Headers();
   myHeaders.append("Client-Id", "<CLIENT-ID>");
   myHeaders.append("Authorization", "Bearer <OAUTH-TOKEN>");
+
+  fetch(`https://api.twitch.tv/helix/streams?user_login=${streamer.name}`, {
+    method: "GET",
+    headers: myHeaders,
+    mode: "cors",
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      setViewCount(
+        new Intl.NumberFormat("en", { notation: "compact" }).format(
+          data.data[0].viewer_count
+        )
+      );
+    });
 
   useEffect(() => {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -27,7 +42,6 @@ export default function TwitchViewcount({ streamer, setViewCount }: Props) {
               data.data[0].viewer_count
             )
           );
-          console.log(data.data[0].viewer_count);
         });
     }, 10000);
   }, []);
