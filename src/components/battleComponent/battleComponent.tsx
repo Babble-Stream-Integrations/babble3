cf50589ad2f2df38f2aa524360ffd219de37d57a
Lvdwardt <87395969+Lvdwardt@users.noreply.github.com>
2022-09-23 15:04:37 +0200

    start quizcomponent

---
+++ src/components/battleComponent/battleComponent.tsx
@@ -0,0 +1,9 @@
+export default function BattleComponent() {
+  return (
+    <div className="bg-red-500">
+      <div className=" bg-babbleGray p-4">
+        <div className="">Wich is the largest city in Morocco?</div>
+      </div>
+    </div>
+  );
+}
