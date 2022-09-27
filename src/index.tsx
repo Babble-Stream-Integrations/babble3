00523ec901e29cfcf5d55df5386b108628434756
Lvdwardt <87395969+Lvdwardt@users.noreply.github.com>
2022-09-27 22:24:50 +0200

    made login page work with dynamic streamer name

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,11 @@
-import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./global.css";
-import Quiz from "./pages/quiz";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   // <React.StrictMode>
   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/quiz" element={<Quiz />} />
-    </Routes>
+    <App />
   </BrowserRouter>,
   // </React.StrictMode>,
   document.getElementById("root")
