24aeb7e3e9b3e1f61d8330bfe1783c50ccd67851
srphihung <sr.phihung@gmail.com>
2022-09-27 10:03:41 +0200

    Creating Timer comonent

---
+++ src/components/timerComponent/timerComponent.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+export default function Timer() {
+  const [days, setDays] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+
+  return;
+  <div className="timer"></div>;
+}
