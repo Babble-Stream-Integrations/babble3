ffae3450029d4f41003a9527f3a042c4aec0330f
Lvdwardt <87395969+Lvdwardt@users.noreply.github.com>
2022-09-27 14:25:08 +0200

    make length of colorbar dynamic

--- src/components/quizComponent/quizComponent.tsx
+++ src/components/quizComponent/quizComponent.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import "./quizComponent.css";
-import { Streamer } from "../../pages/quiz";
 
 export type Quiz = {
   qAmount: number;
@@ -14,6 +13,29 @@ export type Question = {
 export type Answer = {
   answer: string;
 };
+
+//calculate width based on the percentage of people that gave that answer
+function Width(index: number) {
+  type answered = {
+    percent: number;
+  };
+  const [answered] = useState<answered[]>([
+    {
+      percent: 0,
+    },
+    {
+      percent: 0,
+    },
+    {
+      percent: 0,
+    },
+    {
+      percent: 0,
+    },
+  ]);
+  const percentage = answered[index].percent + 10;
+  return `${percentage}%`;
+}
 
 export default function QuizComponent() {
   const [quiz] = useState<Quiz>({
@@ -37,26 +59,9 @@ export default function QuizComponent() {
           },
         ],
       },
-      {
-        qNumber: 2,
-        question: "What is the capital of the United States?",
-        answers: [
-          {
-            answer: "New York",
-          },
-          {
-            answer: "Washington D.C.",
-          },
-          {
-            answer: "Los Angeles",
-          },
-          {
-            answer: "Chicago",
-          },
-        ],
-      },
     ],
   });
+
   return (
     // display the question and answers
     <div className="flex h-full w-[570px] flex-col gap-[10px] overflow-hidden pt-[50px] text-center text-[30px] font-[500] text-white">
@@ -77,18 +82,20 @@ export default function QuizComponent() {
       {/* map over possible answers */}
       {quiz.questions[0].answers.map((answer, index) => {
         const letter = String.fromCharCode(65 + index);
+
         return (
           <div
             key={index}
-            className="flex h-[75px] items-center overflow-hidden rounded-lg rounded-bl-3xl bg-babbleDarkGray text-center"
+            className="relative flex h-[75px] items-center overflow-hidden rounded-lg rounded-bl-3xl bg-babbleDarkGray text-center"
           >
             <div
-              className="min-w-[80px]  rounded-lg rounded-bl-3xl p-4 font-[1000] italic"
+              className="absolute z-0 min-w-[80px] rounded-lg rounded-bl-3xl p-4 pl-7 text-left font-[1000] italic"
               id={letter}
+              style={{ width: Width(index) }}
             >
               <h1>{letter}</h1>
             </div>
-            <div className="w-full pr-[75px] text-[20px]">{answer.answer}</div>
+            <div className="z-10 w-full pl-20 text-[20px]">{answer.answer}</div>
           </div>
         );
       })}
