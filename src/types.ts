e98f26b19d00dd7efb075ee5858a617b06837017
Lvdwardt <87395969+Lvdwardt@users.noreply.github.com>
2022-10-04 19:55:15 +0200

    Created Trivia Settings type

--- src/types.ts
+++ src/types.ts
@@ -11,25 +11,38 @@ export type Streamer = {
   id: string;
 };
 
-//type for Quiz
+//types for Quiz
+export type TriviaSettings = {
+  streamer: string;
+  startAfter: number;
+  questionAmount: number;
+  timePerQuestion: number;
+  timeInBetween: number;
+};
+
+export type Percentages = {
+  percentage: number;
+};
+
 export type QuizBackend = {
   question: string;
   possibilities: string[];
   time: number;
   rightAnswer: string;
   percentages: Percentages[];
+  //TODO questionNumber: number;
 };
 
-export type Percentages = {
-  percentage: number;
-};
 export type QuizComponentData = {
+  qAmount: number;
   question: string;
   answers: string[];
   rightAnswer: string;
   percentages: Percentages[];
+  //TODO questionNumber: number;
 };
 
+//type for timer
 export type TimeProp = {
   timeProp: {
     time: number;
