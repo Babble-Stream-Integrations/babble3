9b8b6b2ebdc9e2aa1b462e5baa2ed430a6b96143
Lvdwardt <87395969+Lvdwardt@users.noreply.github.com>
2022-10-11 20:03:52 +0200

    rewrote index.tsx to react 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,12 @@
 import { createRoot } from "react-dom/client";
-
-import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-const container = document.getElementById("root");
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-const root = createRoot(container!);
+import App from "./App";
 
+const container = document.getElementById("root");
+if (!container) throw new Error("Failed to find the root element");
+const root = createRoot(container);
 root.render(
-  // <React.StrictMode>
   <BrowserRouter>
     <App />
   </BrowserRouter>
-  // </React.StrictMode>,
 );
