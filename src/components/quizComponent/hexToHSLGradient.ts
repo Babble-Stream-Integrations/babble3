24b95b16fdb4f1b580c68f9ecf149b4d3d78f150
Lvdwardt <87395969+Lvdwardt@users.noreply.github.com>
2022-10-11 20:08:03 +0200

    made hex to hsl logic more efficient

--- src/components/quizComponent/hexToHSLGradient.ts
+++ src/components/quizComponent/hexToHSLGradient.ts
@@ -1,48 +1,56 @@
-//code from https://css-tricks.com/converting-color-spaces-in-javascript/#aa-hex-to-hsl
-export default function hexToHSL(H: string, direction: string, hue: string) {
-  // Convert hex to RGB first
-  let r: any = 0,
-    g: any = 0,
-    b: any = 0;
-  if (H.length == 4) {
-    r = "0x" + H[1] + H[1];
-    g = "0x" + H[2] + H[2];
-    b = "0x" + H[3] + H[3];
-  } else if (H.length == 7) {
-    r = "0x" + H[1] + H[2];
-    g = "0x" + H[3] + H[4];
-    b = "0x" + H[5] + H[6];
+export default function hexToHSL(hex: string, direction: string, hue: string) {
+  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  if (!result) {
+    throw new Error("Could not parse Hex Color");
   }
-  // Then to HSL
-  r /= 255;
-  g /= 255;
-  b /= 255;
-  const cmin = Math.min(r, g, b);
-  const cmax = Math.max(r, g, b);
-  const delta = cmax - cmin;
-  let h = 0;
-  let s = 0;
-  let l = 0;
 
-  if (delta == 0) h = 0;
-  else if (cmax == r) h = ((g - b) / delta) % 6;
-  else if (cmax == g) h = (b - r) / delta + 2;
-  else h = (r - g) / delta + 4;
+  const rHex = parseInt(result[1], 16);
+  const gHex = parseInt(result[2], 16);
+  const bHex = parseInt(result[3], 16);
 
-  h = Math.round(h * 60);
+  const r = rHex / 255;
+  const g = gHex / 255;
+  const b = bHex / 255;
 
-  if (h < 0) h += 360;
+  const max = Math.max(r, g, b);
+  const min = Math.min(r, g, b);
 
-  l = (cmax + cmin) / 2;
-  s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
-  s = +(s * 100).toFixed(1);
-  l = +(l * 100).toFixed(1);
+  let h = (max + min) / 2;
+  let s = h;
+  let l = h;
+
+  if (max === min) {
+    // Achromatic
+    h = 0;
+    s = 0;
+  }
+
+  const d = max - min;
+  s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+  switch (max) {
+    case r:
+      h = (g - b) / d + (g < b ? 6 : 0);
+      break;
+    case g:
+      h = (b - r) / d + 2;
+      break;
+    case b:
+      h = (r - g) / d + 4;
+      break;
+  }
+  h /= 6;
+
+  s = s * 100;
+  s = Math.round(s);
+  l = l * 100;
+  l = Math.round(l);
+  h = Math.round(360 * h);
 
   const hsl = "hsl(" + h + "," + s + "%," + l + "%)";
   //make another color by changing the lightness depending on the hue
   const lChange = l + (hue === "lighter" ? 20 : -20);
   const hslChanged = `hsl(${h},${s}%,${lChange}%)`;
 
-  // console.log(`linear-gradient(to right, ${hsl} , ${lightColor})`);
   return `linear-gradient(to ${direction}, ${hsl} , ${hslChanged})`;
 }
+//code for hsl from https://www.jameslmilner.com/posts/converting-rgb-hex-hsl-colors/#hex-to-hsl
