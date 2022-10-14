d4b1d2190c2027e7ac074ae427ffdca10f674260
Lvdwardt <87395969+Lvdwardt@users.noreply.github.com>
2022-10-15 00:15:43 +0200

    removed unused vars

--- src/components/chatComponent/youtubeChat.tsx
+++ src/components/chatComponent/youtubeChat.tsx
@@ -1,6 +1,5 @@
-import React, { useEffect } from "react";
-import axios from "axios";
-import { Message, Streamer } from "../../types";
+import React from "react";
+import { Message } from "../../types";
 
 type Props = {
   liveChatId: string;
