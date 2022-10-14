4f3e0bb25d267d60a1848f7de562df82f7f910d7
Lvdwardt <87395969+Lvdwardt@users.noreply.github.com>
2022-10-14 20:18:54 +0200

    fixed type import in front-end

--- src/components/chatComponent/youtubeChat.tsx
+++ src/components/chatComponent/youtubeChat.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import axios from "axios";
-import { Message } from "../../pages/quiz/quiz";
-import { Streamer } from "../../pages/quiz/quiz";
+import { Message, Streamer } from "../../types";
 
 type Props = {
   liveChatId: string;
