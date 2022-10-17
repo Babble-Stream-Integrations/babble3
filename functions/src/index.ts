825413ae68bb631ffecbc40683a578a8cc029ef1
Lvdwardt <87395969+Lvdwardt@users.noreply.github.com>
2022-10-17 12:00:00 +0200

    changed use to get

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,14 +3,7 @@ import * as functions from "firebase-functions";
 
 // Import and set-up Express
 import express from "express";
-// import cors from "cors";
 const app = express();
-
-// Import routing files
-import viewCountRoutes from "./routes/viewCount";
-import oAuth2Routes from "./routes/oauth2";
-
-// Declare routes
 app.use((req, res, next) => {
   res.append("Access-Control-Allow-Origin", ["*"]);
   res.append("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
@@ -18,8 +11,13 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get("/view-count", viewCountRoutes);
-app.get("/oauth2", oAuth2Routes);
+// Import routing files
+import viewCountRoutes from "./routes/viewCount";
+import oAuth2Routes from "./routes/oauth2";
+
+// Declare routes
+app.use("/view-count", viewCountRoutes);
+app.use("/oauth2", oAuth2Routes);
 
 // eslint-disable-next-line no-undef
 export default functions.region("europe-west1").https.onRequest(app);
