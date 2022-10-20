65170b3553300e2b5a858b005e6c8f130903c9d9
Hiddeeeh <hidde.linderse@gmail.com>
2022-10-20 15:52:24 +0200

    shadows on home page
    Co-authored-by: Lvdwardt <Lvdwardt@users.noreply.github.com>

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,5 +1,5 @@
 import { ImTwitch, ImYoutube } from "react-icons/im";
-import { IoLogoTiktok } from "react-icons/io5";
+//import { IoLogoTiktok } from "react-icons/io5";
 import { useState } from "react";
 import FooterComponent from "../components/footerComponent/footerComponent";
 import { DefaultButton } from "../components/defaultButton/defaultButton";
@@ -19,35 +19,30 @@ export default function Login() {
   //define different sign in methods
   const signinOptions = [
     {
-      platform: "twitch",
-      icon: <ImTwitch size={50} />,
-    },
-    {
       platform: "youtube",
       icon: <ImYoutube size={50} />,
     },
     {
-      platform: "tiktok",
-      icon: <IoLogoTiktok size={50} />,
+      platform: "twitch",
+      icon: <ImTwitch size={50} />,
     },
+    //    {
+    //      platform: "tiktok",
+    //      icon: <IoLogoTiktok size={50} />,
+    //    },
   ];
 
   return (
-    <div className="flex min-h-screen w-screen items-center bg-gradient-radial from-[#2D2D31] via-[#1A1A1D] to-[#1A1A1D]">
-      <div className="py-8 pl-[140px] font-thin">
-        <h1 className="mb-[25px] animate-fade-in-text-0 text-5xl font-normal text-babbleWhite">
-          Welcome,
+    <div className="flex min-h-screen w-screen items-center justify-center bg-gradient-radial from-[#202024] to-[#0E0E10]">
+      <div className="flex flex-col items-center justify-center py-8 font-thin">
+        <h1 className="mb-[25px] animate-fade-in-text-0 text-center text-5xl font-normal text-babbleWhite ">
+          Welcome
         </h1>
         <div className="flex animate-fade-in-text-1">
-          <p className="text-xl text-babbleGray">select your&nbsp;</p>
-          <p className="mb-[50px] text-xl text-babbleLightGray">
-            streaming platform
+          <p className="text-center text-xl text-babbleGray">
+            select your main&nbsp;
           </p>
-        </div>
-        <div className="flex items-center">
-          <div className="h-[2px] w-[8px] rounded-full bg-babbleLightGray" />
-          <div className="h-[1px] w-full rounded-full bg-babbleGray" />
-          <div className="h-[2px] w-[8px] rounded-full bg-babbleLightGray" />
+          <p className="text-xl text-babbleLightGray">streaming platform</p>
         </div>
         <div className="flex gap-[50px] py-[50px]">
           {signinOptions.map((options, index) => {
@@ -61,12 +56,14 @@ export default function Login() {
                   onChange={() => setPlatform(platform)}
                 />
                 <div
-                  className={`relative flex h-[200px] w-[200px] cursor-pointer animate-fade-in-${index} items-center justify-center gap-2 overflow-hidden rounded-babble border border-babbleGray text-babbleGray transition duration-300 hover:bg-gradient-to-tr hover:border-${platform} hover:text-white hover:from-${platform}Dark/10 hover:to-${platform}Light/30 peer-checked:border-2 peer-checked:border-${platform} peer-checked:bg-gradient-to-tr peer-checked:from-${platform}Dark/10 peer-checked:to-${platform}Light/30 peer-checked:text-babbleLightGray`}
+                  className={`group relative flex h-[200px] w-[200px] cursor-pointer shadow-babble animate-fade-in-${index} items-center justify-center gap-2 overflow-hidden rounded-babble border border-babbleGray bg-babbleGray/5 text-babbleGray transition duration-300 hover:border-${platform} hover:bg-gradient-to-br hover:text-white peer-checked:border-2 peer-checked:border-${platform} peer-checked:bg-gradient-to-br peer-checked:from-${platform}Light/30 peer-checked:to-${platform}Dark/10 peer-checked:text-babbleLightGray`}
                 >
                   <div
-                    className={`absolute inset-0 h-full w-full bg-gradient-to-tr from-${platform}Dark/10 to-${platform}Light/30  opacity-0 transition duration-300 hover:opacity-100`}
+                    className={`absolute inset-0 h-full w-full bg-gradient-to-br shadow-babble from-${platform}Light/30  to-${platform}Dark/10 opacity-0 transition duration-300 hover:opacity-100 group-hover:opacity-100`}
                   />
-                  <div className="z-10 flex items-center justify-center">
+                  <div
+                    className={`z-10 flex items-center justify-center [filter:_drop-shadow(3px_2px_4px_#000000);]`}
+                  >
                     {options.icon}
                   </div>
                 </div>
@@ -74,16 +71,11 @@ export default function Login() {
             );
           })}
         </div>
-        <div className="mb-[50px] flex items-center">
-          <div className="h-[2px] w-[8px] rounded-full bg-babbleLightGray" />
-          <div className="h-[1px] w-full rounded-full bg-babbleGray" />
-          <div className="h-[2px] w-[8px] rounded-full bg-babbleLightGray" />
-        </div>
-        <h1 className="w-[600px] animate-fade-in-text-2 text-xl text-babbleGray">
-          You will be asked to verify your account with us. After that you will
-          be redirected to this site.
+        <h1 className="w-[600px] animate-fade-in-text-2 text-center text-xl text-babbleGray">
+          You will be asked to verify your account. After which you <br /> will
+          be redirected back to us.
         </h1>
-        <div className="py-[50px]">
+        <div className="py-[75px]">
           <DefaultButton
             text="Continue"
             buttonClick={() => buttonClicked(platform)}
@@ -91,7 +83,6 @@ export default function Login() {
           />
         </div>
       </div>
-      <FooterComponent />
     </div>
   );
 }
