6505fbc1631875582418930e0101fa00db296e82
Lvdwardt <87395969+Lvdwardt@users.noreply.github.com>
2022-11-08 19:00:12 +0100

    start redesign announcementfeedcomponent

--- src/components/announcementFeedComponent/announcementFeedComponent.tsx
+++ src/components/announcementFeedComponent/announcementFeedComponent.tsx
@@ -1,33 +1,48 @@
+import { FaClock, FaFire, FaTrophy } from "react-icons/fa";
+
 type firstToGuess = {
   firstToGuess: string;
 };
 export default function AnnouncementFeedComponent({
   firstToGuess,
 }: firstToGuess) {
+  console.log(firstToGuess);
   return (
-    <div className="h-full w-[600px] rounded-babble border border-babbleGray bg-babbleGray/5 p-4">
-      <div className="mb-[15px] flex h-[75px] items-center gap-2 rounded-babbleSmall bg-babbleDarkerGray p-8 text-3xl text-babbleWhite shadow-babble backdrop-blur-babble">
-        <h1 className=" whitespace-nowrap text-[20px]">First to Answer:</h1>
-        <div className="relative left-6 mr-8 flex h-10 w-full items-center overflow-hidden rounded-full bg-babbleGray/20 pl-4">
-          <h1> {firstToGuess}</h1>
-        </div>
-      </div>
-      <div className="mb-[15px] flex h-[75px] items-center gap-2 rounded-babbleSmall bg-babbleDarkerGray p-8 text-3xl text-babbleWhite shadow-babble backdrop-blur-babble">
-        <h1 className=" whitespace-nowrap text-[20px]">First to Answer:</h1>
-        <div className="relative left-6 mr-8 flex h-10 w-full items-center overflow-hidden rounded-full bg-babbleGray/20 pl-4">
-          <h1> {firstToGuess}</h1>
-        </div>
-      </div>
-      <div className="mb-[15px] flex h-[75px] items-center gap-2 rounded-babbleSmall bg-babbleDarkerGray p-8 text-3xl text-babbleWhite shadow-babble backdrop-blur-babble">
-        <h1 className=" whitespace-nowrap text-[20px]">First to Answer:</h1>
-        <div className="relative left-6 mr-8 flex h-10 w-full items-center overflow-hidden rounded-full bg-babbleGray/20 pl-4">
-          <h1> {firstToGuess}</h1>
-        </div>
-      </div>
-      <div className="mb-[15px] flex h-[75px] items-center gap-2 rounded-babbleSmall bg-babbleDarkerGray p-8 text-3xl text-babbleWhite shadow-babble backdrop-blur-babble">
-        <h1 className=" whitespace-nowrap text-[20px]">First to Answer:</h1>
-        <div className="relative left-6 mr-8 flex h-10 w-full items-center overflow-hidden rounded-full bg-babbleGray/20 pl-4">
-          <h1> {firstToGuess}</h1>
+    <div className="h-full w-[600px] rounded-babble border border-babbleGray bg-babbleGray/5 p-4 text-3xl text-babbleWhite">
+      <div className="flex items-center justify-between">
+        <div className="flex w-full flex-col">
+          <div className="flex w-full items-center justify-between gap-2 rounded-babbleSmall bg-babbleDarkerGray p-4 backdrop-blur-babble">
+            <FaTrophy />
+            <div className="flex">
+              <h3 className=" text-lg">MOST POINTS: </h3>
+              <h3 className=" text-lg"> {" Leon"}</h3>
+            </div>
+            <FaTrophy />
+          </div>
+          <div className="flex w-full items-center justify-between gap-2 rounded-babbleSmall bg-babbleDarkerGray p-4 backdrop-blur-babble">
+            <FaClock />
+            <div className="flex">
+              <h3 className=" text-lg">FIRST: </h3>
+              <h3 className=" text-lg">{firstToGuess}</h3>
+            </div>
+            <FaClock />
+          </div>
+          <div className="flex w-full items-center justify-between gap-2 rounded-babbleSmall bg-babbleDarkerGray p-4 backdrop-blur-babble">
+            <FaFire />
+            <div className="flex">
+              <h3 className=" text-lg">STREAK: </h3>
+              <h3 className=" text-lg"> {" Leon"}</h3>
+            </div>
+            <FaFire />
+          </div>
+          <div className="flex w-full items-center justify-between gap-2 rounded-babbleSmall bg-babbleDarkerGray p-4 backdrop-blur-babble">
+            <FaTrophy />
+            <div className="flex">
+              <h3 className=" text-lg">MOST POINTS: </h3>
+              <h3 className=" text-lg"> {" Leon"}</h3>
+            </div>
+            <FaTrophy />
+          </div>
         </div>
       </div>
     </div>
