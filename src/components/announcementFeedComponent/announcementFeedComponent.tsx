82bf2cfdd1c80a11a256b3655fe22e77213657ab
Lvdwardt <87395969+Lvdwardt@users.noreply.github.com>
2022-11-10 18:27:23 +0100

    announcement feed component Co-authored-by: srphihung <srphihung@users.noreply.github.com>

--- src/components/announcementFeedComponent/announcementFeedComponent.tsx
+++ src/components/announcementFeedComponent/announcementFeedComponent.tsx
@@ -1,16 +1,21 @@
-import { FaClock, FaFire, FaTrophy } from "react-icons/fa";
-import { IoSpeedometerSharp, IoTimerOutline } from "react-icons/io5";
-import { MdSpeed } from "react-icons/md";
+import { FaTrophy } from "react-icons/fa";
+import { IoSpeedometerSharp } from "react-icons/io5";
 import { AiFillFire } from "react-icons/ai";
 
-type firstToGuess = {
+type AnnouncementFeed = {
+  mostPoints: string;
   firstToGuess: string;
+  onStreak: string;
+  mostPointsAmount: number;
+  onStreakAmount: number;
 };
 export default function AnnouncementFeedComponent({
-  firstToGuess,
-}: firstToGuess) {
+  announcements,
+}: {
+  announcements: AnnouncementFeed;
+}) {
   return (
-    <div className="h-full w-full rounded-babble border border-babbleGray bg-babbleGray/5 p-4 text-3xl text-babbleWhite backdrop-blur-babble">
+    <div className="h-full w-full rounded-babble border border-babbleGray bg-babbleLightGray/5 p-4 text-3xl text-babbleWhite backdrop-blur-babble">
       <div className="flex h-full items-center justify-between">
         <div className="flex h-full w-full flex-col justify-evenly gap-2">
           <div className="flex h-1/4 w-full items-center justify-between gap-2 rounded-babbleSmall bg-babbleDarkerGray p-4 shadow-babble">
@@ -25,7 +30,7 @@ export default function AnnouncementFeedComponent({
             <IoSpeedometerSharp />
             <div className="flex">
               <h3 className=" text-lg">FIRST:</h3>
-              <h3 className=" text-lg">{firstToGuess}</h3>
+              <h3 className=" text-lg">{announcements.firstToGuess}</h3>
             </div>
             <IoSpeedometerSharp />
           </div>
@@ -33,17 +38,17 @@ export default function AnnouncementFeedComponent({
             <div className="relative flex items-center">
               <AiFillFire className="absolute" />
               <h3 className="absolute left-3  pt-2 text-[10px] text-babbleDarkerGray">
-                2
+                {announcements.onStreakAmount}
               </h3>
             </div>
             <div className="flex">
               <h3 className=" text-lg">STREAK:</h3>
-              <h3 className=" text-lg"></h3>
+              <h3 className=" text-lg">{announcements.onStreak}</h3>
             </div>
             <div className="relative flex items-center ">
               <AiFillFire className="absolute right-0" />
               <h3 className="absolute right-3 pt-2 text-[10px] text-babbleDarkerGray">
-                2
+                {announcements.onStreakAmount}
               </h3>
             </div>
           </div>
