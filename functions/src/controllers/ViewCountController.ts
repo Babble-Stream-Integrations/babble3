81cc9588f47a1769849727b79edfe04420bb67ec
Lvdwardt <87395969+Lvdwardt@users.noreply.github.com>
2022-11-24 12:27:47 +0100

    made twitch viewcount call a try-catch

--- functions/src/controllers/ViewCountController.ts
+++ functions/src/controllers/ViewCountController.ts
@@ -24,15 +24,21 @@ import { youtubeConfig } from "../config/youtube";
 
 export async function getViewCount(channel: string, platform: string) {
   if (platform === "twitch") {
-    const result = await axios
-      .get(`https://api.twitch.tv/helix/streams?user_login=${channel}`, {
-        headers: {
-          Authorization: helixConfig.token,
-          "Client-Id": helixConfig.clientId,
-        },
-      })
-      .then((response) => response.data.data[0].viewer_count);
-    return result;
+    try {
+      const result = await axios.get(
+        `https://api.twitch.tv/helix/streams?user_login=${channel}`,
+        {
+          headers: {
+            Authorization: helixConfig.token,
+            "Client-Id": helixConfig.clientId,
+          },
+        }
+      );
+      return result.data.data[0].viewer_count;
+    } catch (error) {
+      console.log(error);
+      return 0;
+    }
   } else if (platform === "youtube") {
     const channelId = await axios({
       method: "GET",
