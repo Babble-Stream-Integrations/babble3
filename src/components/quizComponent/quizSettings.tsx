591cfca6bc015946c962a10fcc72cab0e0cfa964
Hidde <hidde.linderse@gmail.com>
2022-12-02 12:33:06 +0100

    px - rem WIP

--- src/components/quizComponent/quizSettings.tsx
+++ src/components/quizComponent/quizSettings.tsx
@@ -51,21 +51,21 @@ export default function QuizSettings() {
   }
 
   return (
-    <div className="mb-12 flex w-full justify-between px-[100px] text-left">
+    <div className="mb-12 flex w-full justify-between px-[6.25rem] text-left">
       <div className="relative flex">
-        <div className="absolute left-[-50px] h-full -rotate-90 flex-col items-end justify-end ">
-          <h2 className="h-[36px] w-[250px] text-center text-babbleGray">
+        <div className="absolute left-[-3.125rem] h-full -rotate-90 flex-col items-end justify-end ">
+          <h2 className="h-[2.25rem] w-[15.625rem] text-center text-babbleGray">
             Quiz settings
           </h2>
-          <div className="h-px w-[250px] bg-babbleGray"></div>
+          <div className="h-px w-[15.625rem] bg-babbleGray"></div>
         </div>
-        <div className="ph-full z-10 flex w-full flex-col items-start gap-[25px] pr-[15px]  text-babbleGray">
+        <div className="ph-full z-10 flex w-full flex-col items-start gap-[1.563rem] pr-[0.938rem]  text-babbleGray">
           <div>
-            <h2 className="pb-[15px]">Category</h2>
+            <h2 className="pb-[0.938rem]">Category</h2>
             <div>
               <select
                 name="category"
-                className="h-[50px] w-[200px] rounded-lg border-[1px] border-babbleGray bg-babbleDarkGray px-[15px] py-0 text-center text-babbleWhite focus:outline-none"
+                className="h-[3.125 rem] w-[12.5rem] rounded-lg border-[1px] border-babbleGray bg-babbleDarkGray px-[0.938rem] py-0 text-center text-babbleWhite focus:outline-none"
                 id=""
                 value={quizSettings.category}
                 onChange={(e) => handleChange("category", e)}
@@ -81,10 +81,10 @@ export default function QuizSettings() {
             </div>
           </div>
           <div>
-            <h2 className="pb-[15px]">Amount of questions</h2>
+            <h2 className="pb-[0.938rem]">Amount of questions</h2>
             <div>
               <input
-                className="h-[50px] w-[200px] rounded-lg border-[1px] border-babbleGray bg-babbleDarkGray px-[15px] py-0 text-center text-babbleWhite focus:outline-none"
+                className="h-[3.125rem] w-[12.5rem] rounded-lg border-[1px] border-babbleGray bg-babbleDarkGray px-[0.938rem] py-0 text-center text-babbleWhite focus:outline-none"
                 type={"number"}
                 onChange={(e) => handleChange("questionAmount", e)}
                 value={quizSettings.questionAmount}
@@ -94,13 +94,13 @@ export default function QuizSettings() {
             </div>
           </div>
           <div>
-            <h2 className="pb-[15px]">Difficulty</h2>
+            <h2 className="pb-[0.938rem]">Difficulty</h2>
             <div>
               <select
                 onChange={(e) => handleChange("difficulty", e)}
                 value={quizSettings.difficulty}
                 name="difficulty"
-                className="h-[50px] w-[200px] rounded-lg border-[1px] border-babbleGray bg-babbleDarkGray px-[15px] py-0 text-center text-babbleWhite focus:outline-none"
+                className="h-[3.125rem] w-[12.5rem] rounded-lg border-[1px] border-babbleGray bg-babbleDarkGray px-[0.938rem] py-0 text-center text-babbleWhite focus:outline-none"
                 id=""
               >
                 {/* <option value="easy">Easy</option> */}
@@ -110,12 +110,12 @@ export default function QuizSettings() {
             </div>
           </div>
         </div>
-        <div className="flex h-full w-full flex-col items-start gap-[25px]  text-babbleGray">
+        <div className="flex h-full w-full flex-col items-start gap-[1.563rem]  text-babbleGray">
           <div>
-            <h2 className="pb-[15px]">Time to answer (sec)</h2>
+            <h2 className="pb-[0.938rem]">Time to answer (sec)</h2>
             <div>
               <input
-                className="h-[50px] w-[200px] rounded-lg border-[1px] border-babbleGray bg-babbleDarkGray px-[15px] py-0 text-center text-babbleWhite text-babbleWhite focus:outline-none"
+                className="h-[3.125rem] w-[12.5rem] rounded-lg border-[1px] border-babbleGray bg-babbleDarkGray px-[0.938rem] py-0 text-center text-babbleWhite text-babbleWhite focus:outline-none"
                 type={"number"}
                 onChange={(e) => handleChange("timePerQuestion", e)}
                 value={quizSettings.timePerQuestion}
@@ -124,12 +124,12 @@ export default function QuizSettings() {
             </div>
           </div>
           <div>
-            <h2 className="w-[250px] pb-[15px]">
+            <h2 className="w-[15.625rem] pb-[0.938rem]">
               Time till next question (sec)
             </h2>
             <div>
               <input
-                className="h-[50px] w-[200px] rounded-lg border-[1px] border-babbleGray bg-babbleDarkGray px-[15px] py-0 text-center text-babbleWhite focus:outline-none"
+                className="h-[3.125rem] w-[12.5rem] rounded-lg border-[1px] border-babbleGray bg-babbleDarkGray px-[0.938rem] py-0 text-center text-babbleWhite focus:outline-none"
                 type={"number"}
                 onChange={(e) => handleChange("timeInBetween", e)}
                 value={quizSettings.timeInBetween}
@@ -138,8 +138,8 @@ export default function QuizSettings() {
             </div>
           </div>
           <div>
-            <h2 className="pb-[15px]">Elimination</h2>
-            <label className="switch mt-[4px]">
+            <h2 className="pb-[0.938rem]">Elimination</h2>
+            <label className="switch mt-[0.25rem]">
               <input
                 type="checkbox"
                 checked={quizSettings.eliminations}
@@ -150,35 +150,35 @@ export default function QuizSettings() {
           </div>
         </div>
       </div>
-      <div className="relative ml-[100px] flex">
-        <div className="absolute left-[-50px] flex h-full w-full -rotate-90 flex-col items-center px-[50px]">
-          <h2 className="h-[36px] w-[250px] text-center text-babbleGray">
+      <div className="relative ml-[6.25rem] flex">
+        <div className="absolute left-[-3.125rem] flex h-full w-full -rotate-90 flex-col items-center px-[3.125rem]">
+          <h2 className="h-[2.25rem] w-[15.625rem] text-center text-babbleGray">
             Quiz colors
           </h2>
-          <div className="h-px w-[250px] bg-babbleGray"></div>
+          <div className="h-px w-[15.625rem] bg-babbleGray"></div>
         </div>
         <div>
           <div className="flex h-full w-full flex-col items-center justify-center text-babbleWhite">
             {/* //map over uesstate colors, and return letters and colors */}
             {Object.keys(colors).map((letter) => (
-              <div key={letter} className="flex pb-[15px]">
+              <div key={letter} className="flex pb-[0.938rem]">
                 {/* //letter */}
-                <h2 className="mr-[15px] inline-flex w-6 text-3xl  italic text-babbleWhite">
+                <h2 className="mr-[0.938rem] inline-flex w-6 text-3xl  italic text-babbleWhite">
                   {letter.toUpperCase()}
                 </h2>
                 {/* //color */}
                 <input
                   type="text"
-                  className="z-50 mr-[15px] h-[50px] w-[200px] rounded-lg border-[1px] border-babbleGray bg-babbleDarkGray px-[15px] py-0 text-center uppercase focus:outline-none"
+                  className="z-50 mr-[0.938rem] h-[3.125rem] w-[12.5rem] rounded-lg border-[1px] border-babbleGray bg-babbleDarkGray px-[0.938rem] py-0 text-center uppercase focus:outline-none"
                   value={colors[letter]}
                   onChange={(e) => handleColorChange(letter, e)}
                 ></input>
-                <div className="flex h-[50px] w-[50px] items-center justify-center rounded-lg border-[1px] border-babbleGray bg-babbleDarkGray">
+                <div className="flex h-[3.125rem] w-[3.125rem] items-center justify-center rounded-lg border-[1px] border-babbleGray bg-babbleDarkGray">
                   <input
                     type={"color"}
                     onChange={(e) => handleColorChange(letter, e)}
                     value={colors[letter]}
-                    className="h-[36px] w-[36px] cursor-pointer appearance-none border-babbleGray bg-transparent bg-babbleDarkGray text-center focus:outline-none"
+                    className="h-[2.25rem] w-[2.25rem] cursor-pointer appearance-none border-babbleGray bg-transparent bg-babbleDarkGray text-center focus:outline-none"
                   ></input>
                 </div>
               </div>
