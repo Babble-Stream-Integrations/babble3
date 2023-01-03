f20d94dd040e2c5de72ed393b218480fe00ced9a
Lvdwardt <87395969+Lvdwardt@users.noreply.github.com>
2023-01-04 00:49:11 +0100

    remove unused dependencies and update

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dns from "dns";
+
+//open in localhost instead of 127.0.0.1
+dns.setDefaultResultOrder("verbatim");
 
 // https://vitejs.dev/config/
 export default defineConfig({
