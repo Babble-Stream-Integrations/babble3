724291ac347382f0634eaef9d9bb21242bb9cdda
Lvdwardt <87395969+Lvdwardt@users.noreply.github.com>
2023-01-03 15:33:45 +0100

    auto switch between dev and prod environment

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,4 +1,12 @@
-export const appConfig = {
+const prod = {
   backendUrl: "https://backend-sdjmg6ndkq-ew.a.run.app",
   base: "https://europe-west1-babble-d6ef3.cloudfunctions.net/default",
 };
+const dev = {
+  // backendUrl: "https://backend-sdjmg6ndkq-ew.a.run.app",
+  // base: "https://europe-west1-babble-d6ef3.cloudfunctions.net/default",
+  backendUrl: "http://localhost:3001",
+  base: "http://localhost:5001/babble-d6ef3/europe-west1/default",
+};
+
+export const appConfig = process.env.NODE_ENV === "development" ? dev : prod;
