5e6bd0bb629d2b776b459706aec89ac77ecc375c
Lvdwardt <87395969+Lvdwardt@users.noreply.github.com>
2023-01-05 20:26:35 +0100

    pwa support

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { VitePWA } from "vite-plugin-pwa";
 
 import react from "@vitejs/plugin-react";
 import dns from "dns";
@@ -25,7 +26,11 @@ export default defineConfig({
       },
     },
   },
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    VitePWA({ registerType: "autoUpdate", injectRegister: "auto" }),
+  ],
 
   server: {
     port: 3000,
